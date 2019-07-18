507d4f9082142ca6b5c2287eea7a544fa2ac5f41
Kiran Challa <kichalla@microsoft.com>
2019-07-18 14:47:51 -0700

    Added a crashing app

---
+++ foobar/crashingapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace crashingapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            throw new InvalidOperationException("booooo");
+        }
+    }
+}
