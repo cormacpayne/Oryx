5cc3c270a32330e203a8725e86cf6cb06713ac3f
Kiran Challa <kichalla@microsoft.com>
2019-07-18 12:13:36 -0700

    temporarily disable publishing test results to a file

--- build/testBuildImages.sh
+++ build/testBuildImages.sh
@@ -26,4 +26,4 @@ fi
 echo
 echo "Building and running tests..."
 cd "$TESTS_SRC_DIR/$testProjectName"
-dotnet test --test-adapter-path:. --logger:"xunit;LogFilePath=$TEST_RESULTS_DIR/$testProjectName.xml" -c $BUILD_CONFIGURATION
+dotnet test -c $BUILD_CONFIGURATION
