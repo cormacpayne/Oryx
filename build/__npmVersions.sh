285daa9812b6327ce58b6d30f8e86a9964dfca8c
Kiran Challa <kichalla@microsoft.com>
2019-08-02 15:01:24 -0700

    initial

---
+++ build/__npmVersions.sh
@@ -0,0 +1,4 @@
+# This file was auto-generated from 'constants.yaml'. Changes may be overridden.
+
+NPM6_VERSION='6.10.2'
+NPM5_VERSION='5.6.0'
---
+++ src/BuildScriptGenerator/Node/NpmVersions.cs
@@ -0,0 +1,10 @@
+// This file was auto-generated from 'constants.yaml'. Changes may be overridden.
+
+namespace Microsoft.Oryx.BuildScriptGenerator.Node
+{
+    public static class NpmVersions
+    {
+        public const string Npm6Version = "6.10.2";
+        public const string Npm5Version = "5.6.0";
+    }
+}
