5760c78312ba525c4d84912cc36a58f7c45162b0
Kiran Challa <kichalla@microsoft.com>
2019-08-19 05:14:34 -0700

    updated

--- images/build/installDotNetCore.sh
+++ images/build/installDotNetCore.sh
@@ -6,6 +6,19 @@
 
 set -e
 
+containsVersion () {
+    local versionToLookFor="$1"
+    for version in "$@"
+    do
+        if [ "$versionToLookFor" == "" ]
+        then
+            return 0
+        else
+            return 1
+        fi
+    done
+}
+
 apt-get update \
     && apt-get install -y --no-install-recommends \
         libc6 \
@@ -32,32 +45,43 @@ chmod +x /tmp/installDotNetCoreSdk.sh
 # Check https://www.microsoft.com/net/platform/support-policy for support policy of .NET Core versions
 
 source . /tmp/__dotNetCoreSdkVersions.sh
+source . /tmp/__dotNetCoreRunTimeVersions.sh
 
-DOTNET_SDK_VER=$DOT_NET_CORE_11_SDK_VERSION \
-DOTNET_SDK_SHA=$DOT_NET_CORE_11_SDK_SHA512 \
-DOTNET_SDK_URL=https://dotnetcli.blob.core.windows.net/dotnet/Sdk/$DOTNET_SDK_VER/dotnet-dev-debian.9-x64.$DOTNET_SDK_VER.tar.gz \
-# To save disk space do not install packages for this old version which is soon going to be out of support
-INSTALL_PACKAGES=false \
-/tmp/installDotNetCoreSdk.sh
+if containsVersion "$DOT_NET_CORE_11_SDK_VERSION"
+then
+    DOTNET_SDK_VER=$DOT_NET_CORE_11_SDK_VERSION \
+    DOTNET_SDK_SHA=$DOT_NET_CORE_11_SDK_SHA512 \
+    DOTNET_SDK_URL=https://dotnetcli.blob.core.windows.net/dotnet/Sdk/$DOTNET_SDK_VER/dotnet-dev-debian.9-x64.$DOTNET_SDK_VER.tar.gz \
+    # To save disk space do not install packages for this old version which is soon going to be out of support
+    INSTALL_PACKAGES=false \
+    /tmp/installDotNetCoreSdk.sh
+fi
 
-DOTNET_SDK_VER=$DOT_NET_CORE_21_SDK_VERSION \
-DOTNET_SDK_SHA=$DOT_NET_CORE_21_SDK_SHA512 \
-/tmp/installDotNetCoreSdk.sh
+if containsVersion "$DOT_NET_CORE_21_SDK_VERSION"
+then
+    DOTNET_SDK_VER=$DOT_NET_CORE_21_SDK_VERSION \
+    DOTNET_SDK_SHA=$DOT_NET_CORE_21_SDK_SHA512 \
+    /tmp/installDotNetCoreSdk.sh
+fi
 
-DOTNET_SDK_VER=$DOT_NET_CORE_22_SDK_VERSION \
-DOTNET_SDK_SHA=$DOT_NET_CORE_22_SDK_SHA512 \
-/tmp/installDotNetCoreSdk.sh
+if containsVersion "$DOT_NET_CORE_22_SDK_VERSION"
+then
+    DOTNET_SDK_VER=$DOT_NET_CORE_22_SDK_VERSION \
+    DOTNET_SDK_SHA=$DOT_NET_CORE_22_SDK_SHA512 \
+    /tmp/installDotNetCoreSdk.sh
+fi
 
-DOTNET_SDK_VER=$DOT_NET_CORE_30_SDK_VERSION_PREVIEW_NAME \
-DOTNET_SDK_SHA=$DOT_NET_CORE_30_SDK_SHA512 \
-/tmp/installDotNetCoreSdk.sh
+if containsVersion "$DOT_NET_CORE_30_SDK_VERSION"
+then
+    DOTNET_SDK_VER=$DOT_NET_CORE_30_SDK_VERSION_PREVIEW_NAME \
+    DOTNET_SDK_SHA=$DOT_NET_CORE_30_SDK_SHA512 \
+    /tmp/installDotNetCoreSdk.sh
+fi
 
-RUN set -ex \
-    rm -rf /tmp/NuGetScratch \
-    && find /var/nuget -type d -exec chmod 777 {} \;
+rm -rf /tmp/NuGetScratch \
+&& find /var/nuget -type d -exec chmod 777 {} \;
 
-RUN set -ex \
- && sdksDir=/opt/dotnet/sdks \
+sdksDir=/opt/dotnet/sdks \
  && cd $sdksDir \
  && ln -s 1.1 1 \
  && ln -s 2.1 2 \
@@ -67,37 +91,51 @@ dotnetDir=/opt/dotnet \
  && sdksDir=$dotnetDir/sdks \
  && runtimesDir=$dotnetDir/runtimes \
  && mkdir -p $runtimesDir \
- && cd $runtimesDir \
- && . /tmp/__dotNetCoreSdkVersions.sh \
- && . /tmp/__dotNetCoreRunTimeVersions.sh \
- # 1.1 sdk <-- 1.0 runtime's sdk
- && mkdir $NET_CORE_APP_10 \
- && ln -s $NET_CORE_APP_10 1.0 \
- && ln -s $sdksDir/$DOT_NET_CORE_11_SDK_VERSION $NET_CORE_APP_10/sdk \
- # 1.1 sdk <-- 1.1 runtime's sdk
- && mkdir $NET_CORE_APP_11 \
- && ln -s $NET_CORE_APP_11 1.1 \
- && ln -s 1.1 1 \
- && ln -s $sdksDir/$DOT_NET_CORE_11_SDK_VERSION $NET_CORE_APP_11/sdk \
- # 2.1 sdk <-- 2.0 runtime's sdk
- && mkdir $NET_CORE_APP_20 \
- && ln -s $NET_CORE_APP_20 2.0 \
- && ln -s $sdksDir/$DOT_NET_CORE_21_SDK_VERSION $NET_CORE_APP_20/sdk \
- # 2.1 sdk <-- 2.1 runtime's sdk
- && mkdir $NET_CORE_APP_21 \
- && ln -s $NET_CORE_APP_21 2.1 \
- && ln -s 2.1 2 \
- && ln -s $sdksDir/$DOT_NET_CORE_21_SDK_VERSION $NET_CORE_APP_21/sdk \
- # 2.2 sdk <-- 2.2 runtime's sdk
- && mkdir $NET_CORE_APP_22 \
- && ln -s $NET_CORE_APP_22 2.2 \
- && ln -s $sdksDir/$DOT_NET_CORE_22_SDK_VERSION $NET_CORE_APP_22/sdk \
- # 3.0 sdk <-- 3.0 runtime's sdk
- && mkdir $NET_CORE_APP_30 \
- && ln -s $NET_CORE_APP_30 3.0 \
- && ln -s 3.0 3 \
- && ln -s $sdksDir/$DOT_NET_CORE_30_SDK_VERSION $NET_CORE_APP_30/sdk \
- # LTS sdk <-- LTS runtime's sdk
- && ln -s 2.1 lts \
- && ltsSdk=$(readlink lts/sdk) \
- && ln -s $ltsSdk/dotnet /usr/local/bin/dotnet
+ && cd $runtimesDir
+
+if containsVersion "$DOT_NET_CORE_11_SDK_VERSION"
+then
+    # 1.1 sdk <-- 1.0 runtime's sdk
+    mkdir $NET_CORE_APP_10 \
+    && ln -s $NET_CORE_APP_10 1.0 \
+    && ln -s $sdksDir/$DOT_NET_CORE_11_SDK_VERSION $NET_CORE_APP_10/sdk \
+    # 1.1 sdk <-- 1.1 runtime's sdk
+    && mkdir $NET_CORE_APP_11 \
+    && ln -s $NET_CORE_APP_11 1.1 \
+    && ln -s 1.1 1 \
+    && ln -s $sdksDir/$DOT_NET_CORE_11_SDK_VERSION $NET_CORE_APP_11/sdk
+fi
+
+if containsVersion "$DOT_NET_CORE_21_SDK_VERSION"
+then
+    # 2.1 sdk <-- 2.0 runtime's sdk
+    mkdir $NET_CORE_APP_20 \
+    && ln -s $NET_CORE_APP_20 2.0 \
+    && ln -s $sdksDir/$DOT_NET_CORE_21_SDK_VERSION $NET_CORE_APP_20/sdk \
+    # 2.1 sdk <-- 2.1 runtime's sdk
+    && mkdir $NET_CORE_APP_21 \
+    && ln -s $NET_CORE_APP_21 2.1 \
+    && ln -s 2.1 2 \
+    && ln -s $sdksDir/$DOT_NET_CORE_21_SDK_VERSION $NET_CORE_APP_21/sdk
+fi
+
+if containsVersion "$DOT_NET_CORE_22_SDK_VERSION"
+then
+    # 2.2 sdk <-- 2.2 runtime's sdk
+    mkdir $NET_CORE_APP_22 \
+    && ln -s $NET_CORE_APP_22 2.2 \
+    && ln -s $sdksDir/$DOT_NET_CORE_22_SDK_VERSION $NET_CORE_APP_22/sdk
+fi
+
+if containsVersion "$DOT_NET_CORE_30_SDK_VERSION"
+then
+    # 3.0 sdk <-- 3.0 runtime's sdk
+    mkdir $NET_CORE_APP_30 \
+    && ln -s $NET_CORE_APP_30 3.0 \
+    && ln -s 3.0 3 \
+    && ln -s $sdksDir/$DOT_NET_CORE_30_SDK_VERSION $NET_CORE_APP_30/sdk \
+    # LTS sdk <-- LTS runtime's sdk
+    && ln -s 2.1 lts \
+    && ltsSdk=$(readlink lts/sdk) \
+    && ln -s $ltsSdk/dotnet /usr/local/bin/dotnet
+fi
